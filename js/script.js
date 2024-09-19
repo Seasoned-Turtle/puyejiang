e08a778af0c5e56fbbc4215f5fb07683e0d247d8
四季憨龟 <lim301139@qq.com>
2024-09-19 20:11:41 +0800

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,15 @@
+function toggleSidebar() {
+    var sidebar = document.querySelector('.sidebar');
+    var content = document.querySelector('.content');
+    var toggleBtn = document.querySelector('.toggle-btn');
+
+    if (sidebar.style.width === '250px') {
+        sidebar.style.width = '0px';
+        content.style.marginLeft = '0px';
+        toggleBtn.style.left = '0px';
+    } else {
+        sidebar.style.width = '250px';
+        content.style.marginLeft = '260px';
+        toggleBtn.style.left = '250px';
+    }
+}
